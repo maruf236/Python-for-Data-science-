71b79cb7711586bf6e6f223b818c4d95bb914776
Ali Ahsan Maruf <122002aliahsanmaruf@gmail.com>
2026-03-24 02:59:55 +0600

    Update 09_modules.py

---
+++ python for DS/09_modules.py
@@ -0,0 +1,32 @@
+#Module: A module is a Python file that contains
+#  functions, variables, or classes.
+ 
+# Math
+import math 
+a=math.factorial(5)
+print(a)
+
+b=math.floor(4.565)
+print(b)
+
+c=math.sqrt(169)
+print(c)
+
+
+
+
+# Keyword : reserb word by the compiler
+import keyword
+print(keyword.kwlist)
+
+# random
+import random
+print(random.randint(1,100))
+
+
+# Datetime
+import datetime
+print(datetime.datetime.now()) # Current date and time using datetime.now() module
+
+
+help('modules') #to find a the unknow module 
